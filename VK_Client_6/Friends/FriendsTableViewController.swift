8e3bcfd57ffa1df06eb46614b4503575e4bf2acf
Igor Polousov <igorpolousov@gmail.com>
2022-02-03 14:09:59 +0300

    + operations

--- VK_Client_6/Friends/FriendsTableViewController.swift
+++ VK_Client_6/Friends/FriendsTableViewController.swift
@@ -59,13 +59,27 @@ class FriendsTableViewController: UITableViewController, UISearchResultsUpdating
         ]
         
         let url = urlComponents.url!
-        if let data = try? Data(contentsOf: url) {
-            self.parse(json: data)
-            print("FRIENDS NAME")
-            print(friendsName)
-            addToRealmDataBase()
-            //return
-        }
+        
+        let getDataOperation = GetDataOperation(url: url)
+        queue.addOperation(getDataOperation)
+        
+        let parseData = ParseOperation()
+        parseData.addDependency(getDataOperation)
+        queue.addOperation(parseData)
+        
+        let addToRealmDataBase = AddToRealmOperation()
+        addToRealmDataBase.addDependency(parseData)
+        queue.addOperation(addToRealmDataBase)
+        
+        
+        
+        
+//        if let data = try? Data(contentsOf: url) {
+//            self.parse(json: data)
+//            print("FRIENDS NAME")
+//            print(friendsName)
+//            addToRealmDataBase()
+//        }
         
         friendsObserver = getDataFromRealm()
         
