3898f3d10ce6b32d3a32f2ee79668bff78e9d0f2
Igor Polousov <igorpolousov@gmail.com>
2022-02-15 16:18:01 +0300

    + cell file + cell design storyboard

---
+++ VK_Client_6/MyPhotos/HashServise/HashWOXibCell.swift
@@ -0,0 +1,23 @@
+//
+//  HashWOXibCell.swift
+//  VK_Client_6
+//
+//  Created by Igor Polousov on 15.02.2022.
+//
+
+import UIKit
+
+class HashWOXibCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
